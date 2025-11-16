ee11ddfeb5a09a8fd45e8a33c3f03ede94cf1b42
李碧强 <624144061@qq.com>
2025-11-17 00:43:23 +0800

    路由规则

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -2,9 +2,22 @@ export interface Env {
   ASSETS: Fetcher;
 }
 
+async function serveIndex(request: Request, env: Env) {
+  const url = new URL(request.url);
+  const indexUrl = new URL('/', url);
+  return env.ASSETS.fetch(new Request(indexUrl, request));
+}
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
-    return env.ASSETS.fetch(request);
+    const res = await env.ASSETS.fetch(request);
+    if (
+      res.status === 404 &&
+      request.method === 'GET' &&
+      !new URL(request.url).pathname.startsWith('/api/')
+    ) {
+      return serveIndex(request, env);
+    }
+    return res;
   },
 };
-

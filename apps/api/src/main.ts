7330b72e2cfd2b996c28f0d3c26c9e29f3d4f202
李碧强 <624144061@qq.com>
2025-11-18 19:32:54 +0800

    cros

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+    origin: [
+      'https://tapcanvas.beqlee.icu',
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://127.0.0.1:5173',
+      'http://127.0.0.1:5174',
+      // 开发环境允许所有本地地址
+      /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
